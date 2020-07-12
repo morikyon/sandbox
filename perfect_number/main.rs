087731d40a8f016d833caa9a9b128384a11a40db
morikyon <kiyotaka.forest@gmail.com>
2020-07-12 18:36:51 +0900

    first commit

---
+++ perfect_number/main.rs
@@ -0,0 +1,21 @@
+fn divisors(n: usize) -> usize {
+    let mut sum: usize = 0;
+    for i in 1..=(f64::sqrt(n as f64) + 1e-9) as usize {
+        if n % i == 0 {
+            sum += i;
+            if i != n / i {
+                sum += n / i;
+            }
+        }
+    }
+    return sum;
+}
+
+fn main() {
+    for i in 1.. 10000 {
+        let x = divisors(i);
+        if (x - i) == i {
+            println!("It's perfect number: {}", i);
+        }
+    }
+}
